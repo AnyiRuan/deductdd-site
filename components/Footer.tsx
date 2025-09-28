4473545229161b884304377fd57b078b823f95ee
AnyiRuan <168930722+AnyiRuan@users.noreply.github.com>
2025-09-27 20:09:20 -0700

    feat: 删除页脚中的地址信息
    - 从 Footer 组件中移除 '5670 Schaefer Ave, Unit D, Chino CA 91710, USA' 地址
    - 简化页脚布局，只保留版权信息

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,7 +9,6 @@ export default function Footer() {
       <div className="container-x py-10 text-sm text-neutral-500">
         <div className="flex flex-col md:flex-row justify-between gap-4">
           <p>{t('footer.copyright')}</p>
-          <p>5670 Schaefer Ave, Unit D, Chino CA 91710, USA</p>
         </div>
       </div>
     </footer>
